1c1c0db3367dac5bd469c54df2f88cf8276aa6ce
Thomas Stacey <thomas@thomasstacey.co.uk>
2022-12-29 16:45:40 +0000

    Add `fromSet` to `Picture`

--- src/Util/Picture.hs
+++ src/Util/Picture.hs
@@ -1,6 +1,9 @@
 module Util.Picture where
-import Data.Matrix (Matrix)
-import qualified Data.Matrix as Matrix
+import           Data.Bifunctor (bimap)
+import           Data.Matrix    (Matrix)
+import qualified Data.Matrix    as Matrix
+import           Data.Set       (Set)
+import qualified Data.Set       as Set
 
 newtype Picture = Picture {getMatrix :: Matrix Bool}
     deriving Eq
@@ -13,3 +16,13 @@ instance Show Picture where
 
 fromHashDot :: String -> Picture
 fromHashDot = Picture . Matrix.fromLists . map (map (=='#')) . lines
+
+fromSet :: Set (Int, Int) -> Picture
+fromSet s = Picture $ Matrix.matrix rows cols $ (`Set.member` newSet)
+    where
+        minX = Set.findMin $ Set.map fst s
+        minY = Set.findMin $ Set.map snd s
+        newSet = Set.mapMonotonic (bimap ((1+) . subtract minX) ((1+) . subtract minY)) s
+
+        rows = Set.findMax $ Set.map fst newSet
+        cols = Set.findMax $ Set.map snd newSet
